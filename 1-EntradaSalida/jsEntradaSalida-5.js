9deb55e299e47bdc308e3accb6620826ba0dee09
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-07 07:48:41 -0500

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,12 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
-}
+	var name;
+	var age;
+
+	name=document.getElementById("elNombre").value;
+	age=document.getElementById("laEdad").value;
+
+	alert("Usted se llama " + name + " y tiene " + age + " años ");
+} 
 
