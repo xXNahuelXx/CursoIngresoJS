0ef93404440dfb133de4a9e0881bba1cb610107c
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-07 17:35:46 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var num1;
+	var num2;
 	
+	
+	num1=document.getElementById("importe").value;
+	num2=document.getElementById("importe").value;
+	
+	num1=parseInt(num1);
+	num2=parseInt(num1-((num2*25)/100));
+	
+
+
+    document.getElementById("resultado").value=num2;
+
 }
