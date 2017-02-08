197a1bfe1edca6b31ab598c16b8954044ebb7adc
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-08 11:39:02 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,16 @@ function Mostrar()
 {
 //tomo la edad  
 
+var age;
 
+age=document.getElementById("edad").value;
+
+if(age<18)
+{
+	if(age>12)
+	{
+		alert("Usted es un adolescente");
+	}
+}
 
 }//FIN DE LA FUNCIÓN
