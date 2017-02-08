e45f117565b89bbd560d4b6f67ab57c80710bfa6
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-08 11:52:35 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,23 @@
 function Mostrar()
 {
 //tomo la edad  
+var age;
 
+age=document.getElementById("edad").value;
+
+if(age<18&&age>12)
+{
+ 
+}
+else
+{
+	alert("Usted no es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
+
+/*Otra forma de resolver es:
+if(!(age<18&&edad>12))
+{
+	alert("Usted no es adolescente");
+}
