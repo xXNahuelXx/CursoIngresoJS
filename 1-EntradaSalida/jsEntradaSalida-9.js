d4e892a7d614abb3eab3371c28cb53fc470efacf
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-08 09:22:02 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -13,12 +13,21 @@ function MostrarAumento()
 	num2=document.getElementById("sueldo").value;
 	
 	num1=parseInt(num1);
-	num2=parseInt(num1+((num2*10)/100));
+	num2=parseInt(num1+((num2*10)/100));    /*sueldo + sueldo*0.10 (sacamos factor comun) que es=sueldo 
+											  sueldo*1+0.10 (en caso de que fuera 90% sera 0.9) */
+ 
 	
 
 
-    document.getElementById("resultado").value=num2;
+   document.getElementById("resultado").value=num2;
 
 
 }
 
+/* Otra manera de resolver:
+   sueldo=parseInt("Sueldo");
+   resultado=parseInt("Resultado");
+
+   resultado=(sueldo*1+0.10);
+
+   document.getElementById(Resultado).value=resultado; 
