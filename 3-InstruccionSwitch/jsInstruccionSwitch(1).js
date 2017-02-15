c0b5e400213850ba9fab68bbaf21696573d9166e
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-15 09:05:56 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,25 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño)
 
+{
+	case "Enero":
+	alert("Que comiences bien el año");
+	break;
+
+	case "Marzo":
+	alert("A clases");
+	break;
+
+	case "Julio":
+	alert("Se vienen las vacaciones");
+	break;
+
+	case "Diciembre":
+	alert("Felices fiestas ");
+	break;
+}
 
 
 }//FIN DE LA FUNCIÓN
