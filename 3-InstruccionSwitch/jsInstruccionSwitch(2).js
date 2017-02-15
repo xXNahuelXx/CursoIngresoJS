3d9910497daf373b74a4be51ff0cf76bb804754b
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-15 09:06:19 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,31 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	alert("Falta para el invierno");
+	break;
+
+	case "Julio":
+	case "Agosto":
+	alert("Abrigate que hace frio");
+	break;
+
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	alert("Ya pasamos el frio, ahora hace calor");
+	break;
+}
+
+//alert (mesDelAño); antes del switch.
 
 
 
