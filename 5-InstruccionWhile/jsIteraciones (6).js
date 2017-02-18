4bf065530c05f4f00b463171d4b68ead2eaf8baa
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-18 11:23:29 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,6 +3,22 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	var promedio;
+
+	   
+
+    while(contador<5)
+    {
+    	
+    	contador++
+    	numero=prompt("Ingrese numero");
+    	numero=parseInt(numero);
+    	acumulador=acumulador+numero
+
+
+    }
+
 
 
 
