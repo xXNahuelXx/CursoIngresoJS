a0b7d22a381d01805156cf132ab1bb27a2e51126
xXNahuelXx <nahuel.ghigliotti98@gmail.com>
2017-02-21 09:55:12 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,40 @@
 function Mostrar()
-{
-
+{	
 	var contador=0;
-	// declarar variables
-	
+	var maximo;
+	var minimo;
+	var numero;
 	var respuesta='si';
 
-	while(respuesta!='no')
-	{
-		
+while(respuesta!="no")
+{
+	numero=prompt("Ingrese un numero");
+	numero=parseInt(numero);
+	respuesta=prompt("¿Desea continuar?");
 	
+	if(contador==0)
+	{
+		maximo=numero;
+		minimo=numero;
 	}
+	else
+	{
+		if(numero>maximo)
+		{
+			maximo=numero;
+		}
+		if(numero<minimo)
+		{
+			minimo=numero;
+		}	
+	}
+contador++;
+	
+}
 
 
+document.getElementById("maximo").value=maximo;
+document.getElementById("minimo").value=minimo;
 
 
 }//FIN DE LA FUNCIÓN
